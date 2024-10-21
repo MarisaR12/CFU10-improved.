7e42c9b97f972ebf1f8c3b30b49ccbe670bf6e54
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2024-10-21 12:36:51 -0400

    Create cfu10 imporved.py

---
+++ cfu10 imporved.py
@@ -0,0 +1,30 @@
+#Marisa Ramautar 
+#10/21/24
+#period 5 and 6
+#create a guessing game that allows the player to guess 
+#wheather the guess is too high or low and and prints how 
+#many times it took the user to get the number
+
+import random
+#Generate a random number between 1 and 10
+random_num = random.randint(1,10)
+
+#Initialize attempts = 0
+def guess_num(attempt):
+    num_guess = int(input("What number do you think it is between 1 and 10?"))
+    #increase attempts by one
+    attempt += 1
+    #Is the guess equal to random number
+    if num_guess == random_num:
+        print(f"Correct! attempts:{attempt}")
+        #Go to the step where the player is told the number
+    elif  num_guess > random_num:
+        print("Too high! try again.")
+        guess_num(attempt)
+        #if not 
+    else:
+        print("Too low")
+        guess_num(attempt)
+
+        
+guess_num(0)
